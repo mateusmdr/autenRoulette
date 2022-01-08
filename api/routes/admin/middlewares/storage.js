83f5da18aea520f516308e170239697224a24fd4
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-01-08 15:15:39 -0300

    enable caching and remove console.logs

--- api/routes/admin/middlewares/storage.js
+++ api/routes/admin/middlewares/storage.js
@@ -4,8 +4,6 @@ import crypto from 'crypto';
 import multer from 'multer';
 import sharp from 'sharp';
 
-sharp.cache(false);
-
 const uniqueName = () => Date.now().toString() + crypto.randomBytes(16).toString('hex');
 
 const storage = multer.diskStorage({
@@ -25,7 +23,6 @@ export const upload = multer({
         fileSize: 20480000 //Limit size to 20MB
     },
     fileFilter: (req, file, cb) => {
-        console.log(file);
         if(!['image/jpeg','image/png'].includes(file.mimetype))
             return cb(new multer.MulterError({code: 'INVALID_FILE_TYPE', message: "Must be a jpg/png image"}), false);
 
@@ -36,7 +33,6 @@ export const upload = multer({
 const defaultSize = {width: 312, height: 60};
 
 export const resize = async (req, res, next) => {
-    console.log(req.file);
     const filePath = req.file.path;
     let buffer = await sharp(filePath).resize(defaultSize).toBuffer(); //Resize and save on memory
 

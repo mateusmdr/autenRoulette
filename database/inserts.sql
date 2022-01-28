14075be38aed0b50c86b7b29e5d6ed3c5d08d9d3
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-01-28 13:35:34 -0400

    drawNumber should be null if not success

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,15 +1,15 @@
 INSERT INTO availablePrizes (maxDraws, amount, resultType,resetPeriod,drawNumber)
 VALUES
     (15, 5000, 'success', 'weekly', 0),
-    (NULL, NULL, 'retry', NULL, 0),
-    (NULL, NULL, 'fail', NULL, 0),
+    (NULL, NULL, 'retry', NULL, NULL),
+    (NULL, NULL, 'fail', NULL, NULL),
     (15, 5000, 'success', 'weekly', 0),
-    (NULL, NULL, 'retry', NULL, 0),
-    (NULL, NULL, 'fail', NULL, 0),
+    (NULL, NULL, 'retry', NULL, NULL),
+    (NULL, NULL, 'fail', NULL, NULL),
     (15, 5000, 'success', 'weekly', 0),
-    (NULL, NULL, 'retry', NULL, 0),
-    (NULL, NULL, 'fail', NULL, 0),
+    (NULL, NULL, 'retry', NULL, NULL),
+    (NULL, NULL, 'fail', NULL, NULL),
     (15, 5000, 'success', 'weekly', 0),
-    (NULL, NULL, 'retry', NULL, 0),
-    (NULL, NULL, 'fail', NULL, 0)
+    (NULL, NULL, 'retry', NULL, NULL),
+    (NULL, NULL, 'fail', NULL, NULL)
 ;

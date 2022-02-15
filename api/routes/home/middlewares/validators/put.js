d2bfef43e9956f11aefb5f9454e149db0b08bb18
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-02-14 20:50:41 -0400

    don't validate cookie setPixKey

--- api/routes/home/middlewares/validators/put.js
+++ api/routes/home/middlewares/validators/put.js
@@ -2,6 +2,5 @@ import {body} from 'express-validator';
 
 export const setPixKey = [
     body('pixKey', 'Chave PIX inválida').exists().bail().notEmpty().bail()
-    .custom((v, {req}) => !!req.session.drawnPrizeId)
     .blacklist('\-\/\s')
 ]
